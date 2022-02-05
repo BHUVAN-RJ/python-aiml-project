f35de3edc575009b17f3acb89991a81a28d709fa
BHUVAN-RJ <83565908+BHUVAN-RJ@users.noreply.github.com>
2022-02-05 22:57:33 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as ny
+
+datasets= pd.read_csv("/Users/bhuvanrj/Desktop/data science/Advertising.csv")
+
+X=datasets.iloc[:,:-1]
+Y=datasets.iloc[:,3]
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.20,random_state=0)
+
+from sklearn.linear_model import LinearRegression
+model =LinearRegression()
+model.fit(X_train,Y_train)
+y_pred= model.predict(X_test)
+
+from sklearn.metrics import mean_squared_error
+print(mean_squared_error(Y_test,y_pred))
+print(mean_squared_error(Y_test,y_pred,squared=False))
+
+from sklearn.metrics import mean_absolute_error
+print(mean_absolute_error(Y_test,y_pred))
+
