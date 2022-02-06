998bf79b65ad0c94b8fd5ef663e44ebef9bd22f2
BHUVAN-RJ <83565908+BHUVAN-RJ@users.noreply.github.com>
2022-02-06 18:06:53 +0530

    Update assign3.py

--- Advertising_linear_regression_model_test/assign3.py
+++ Advertising_linear_regression_model_test/assign3.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as ny
 from sklearn.datasets import load_breast_cancer
+
 data = load_breast_cancer()
+
 df=pd.DataFrame(data.data, columns = data.feature_names)
 df['target']=data.target
+
+
 X=df.iloc[:,:-1].values
 Y=df.iloc[:,30].values
 
@@ -27,13 +31,14 @@ from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X_train_scaled,Y_train)
 
+//predicting using our model
 y_pred =model.predict(X_test_scaled)
 
 X_test_unscaled=scalar.inverse_transform(X_test_scaled)
 X_train_unscaled=scalar.inverse_transform(X_train_scaled)
 
 
-
+//Metrics to compare effeciency of our regressor model
 from sklearn.metrics import mean_squared_error
 print("MSE",mean_squared_error(Y_test,y_pred))
 print("model score",model.score(X_test_scaled,Y_test))
